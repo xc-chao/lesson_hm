0a68048b38870ce004cfb2a3d6cd23c81a413fc5
xc-chao <2780036289@qq.com>
2024-11-07 17:19:23 +0800

    OpenAI

--- ai/openai/slb/main.mjs
+++ ai/openai/slb/main.mjs
@@ -13,9 +13,9 @@ const openai = new OpenAI({
 const response = await openai.completions.create({
     model:'gpt-3.5-turbo-instruct', // 模型名称
     max_tokens:256, // 生成的最大token数
-    prompt:`假如你是林夕这样的爱情歌曲作词大家，
-        请你写一首100字，主题为爱上森林北的摇滚歌曲。
-        森林北是一位美丽、洒脱、有活力的女孩。`
+    prompt:`假如你是莎士比亚（William Shakespeare）这样的伟大剧作家，
+        请你写一段100左右字，主题为爱上你的crush的表白词。
+        你的crush是一位美丽、洒脱、有活力的女孩。`
 
 })
 
