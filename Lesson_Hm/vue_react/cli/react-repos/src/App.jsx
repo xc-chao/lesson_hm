d939807b559bc4cca3fbda00e1de598ea2e0d976
xc-chao <2780036289@qq.com>
2024-12-21 17:55:23 +0800

    将网站内容以列表呈现出来，组件

---
+++ Lesson_Hm/vue_react/cli/react-repos/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+// es6 模块化引入
+import Repos from './components/repos/repos.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Repos/>
+      </div>
+    </>
+  )
+}
+// es6 模块化导出
+export default App
